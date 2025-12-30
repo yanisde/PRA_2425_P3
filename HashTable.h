6162fb64357fcbe6bded79e828d7295c600bbd8d
YRGHALI <yrghali@alumno.upv.es@ldsic-vdi04.upvnet.upv.es>
2025-12-30 13:30:51 +0100

    Añadida implementación de la clase HashTable

---
+++ HashTable.h
@@ -0,0 +1,83 @@
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+#include <ostream>
+#include <stdexcept>
+#include <string>
+#include "Dict.h"
+#include "TableEntry.h"
+#include "../PRA_2526_P1/ListLinked.h"
+
+
+template <typename V>
+class HashTable : public Dict<V> {
+private:
+    int n;
+    int max;
+    ListLinked<TableEntry<V>>** table;
+
+    int h(std::string key) {
+        int sum = 0;
+        for (size_t i = 0; i < key.size(); i++) sum += int(key.at(i));
+        return sum % max;
+    }
+
+public:
+    HashTable(int size) : n(0), max(size) {
+        if (size <= 0) throw std::runtime_error("Invalid hash table size");
+        table = new ListLinked<TableEntry<V>>*[max];
+        for (int i = 0; i < max; i++) table[i] = new ListLinked<TableEntry<V>>();
+    }
+
+    ~HashTable() {
+        for (int i = 0; i < max; i++) delete table[i];
+        delete[] table;
+    }
+
+    void insert(std::string key, V value) override {
+        int idx = h(key);
+        try {
+            table[idx]->search(TableEntry<V>(key));
+            throw std::runtime_error("Key already exists");
+        } catch (const std::runtime_error&) {
+            table[idx]->insert(TableEntry<V>(key, value));
+            n++;
+        }
+    }
+
+    V search(std::string key) override {
+        int idx = h(key);
+        TableEntry<V> te = table[idx]->search(TableEntry<V>(key));
+        return te.value;
+    }
+
+    V remove(std::string key) override {
+        int idx = h(key);
+        TableEntry<V> te = table[idx]->remove(TableEntry<V>(key));
+        n--;
+        return te.value;
+    }
+
+    int entries() override {
+        return n;
+    }
+
+    int capacity() {
+        return max;
+    }
+
+    V operator[](std::string key) {
+        return search(key);
+    }
+
+    friend std::ostream& operator<<(std::ostream &out, const HashTable<V> &th) {
+        out << "HashTable(n=" << th.n << ", max=" << th.max << ")\n";
+        for (int i = 0; i < th.max; i++) {
+            out << "[" << i << "] " << *(th.table[i]) << "\n";
+        }
+        return out;
+    }
+};
+
+#endif
+
